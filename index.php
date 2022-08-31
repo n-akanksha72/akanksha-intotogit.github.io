6d05a97593ca8a5b1fec8707a4cd1f09d0980ebe
Akanksha <112153954+n-akanksha72@users.noreply.github.com>
2022-08-31 11:55:46 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Jose David Rueda Torres</title>
+        <link type="text/css" rel="stylesheet" href="style.css">
+        <h1>Jose Rueda Resume Registry</h1>
+    </head>
+    <body>
+        <p>
+            <a href="login.php">Please log in</a>
+        </p>
+        <p>
+            <b>Note:</b> Your implementation should retain data across multiple logout/login sessions. 
+            This sample implementation clears all its data periodically - which you should not do in your implementation.
+        </p>
+    </body>
+</html>
